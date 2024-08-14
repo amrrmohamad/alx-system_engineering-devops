a1a4df4d063e1f0586eeaf046c7802be00720b42
amrrmohamad <amr.r.mohamad55@gmail.com>
2024-08-14 22:49:25 +0300

    axl

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# Fixes bad file "wp-settings.php".
+# In web stack Debugging.
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
